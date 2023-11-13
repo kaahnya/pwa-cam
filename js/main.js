ecc3920146250b7cf6c5adf6019364cefe7ad655
kaahnya <karina.decleva@estudante.ifms.edu.br>
2023-11-13 14:20:24 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,46 +1,62 @@
 //registrando a service worker
-if ('serviceWorker' in navigator) 
-{ window.addEventListener('load', async () => {
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
     try {
-    let reg;
-    reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
+      let reg;
+      reg = await navigator.serviceWorker.register('/sw.js', { type: "module" });
 
-    console.log('Service worker registrada!', reg);
+      console.log('Service worker registrada! 😎', reg);
     } catch (err) {
-    console.log('Service worker registro falhou:', err);
+      console.log('😥 Service worker registro falhou: ', err);
     }
   });
 }
-// configurando as constraintes do video stream
-    var constraints = { video: { facingMode: "user" }, audio: false }; 
 
-    // capturando os elementos em tela
-    const cameraView = document.querySelector("#camera--view"), 
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"), 
-    cameraTrigger = document.querySelector("#camera--trigger")
+var camMode = "user";
+// configurando as constraintes do video stream
+var constraints = { video: { facingMode: camMode }, audio: false };
+// capturando os elementos em tela
+const cameraView = document.querySelector("#camera--view"),
+  cameraOutput = document.querySelector("#camera--output"),
+  cameraSensor = document.querySelector("#camera--sensor"),
+  cameraTrigger = document.querySelector("#camera--trigger"),
+  cameraSwitcher = document.querySelector("#camera--switch");
 
 //Estabelecendo o acesso a camera e inicializando a visualização
 function cameraStart() {
-    navigator.mediaDevices
+  navigator.mediaDevices
     .getUserMedia(constraints)
     .then(function (stream) {
-      track = stream.getTracks()[0]; 
+      let track = stream.getTracks()[0]
       cameraView.srcObject = stream;
     })
-      .catch(function (error) {
-       console.error("Ocorreu um Erro.", error);
-      });
+    .catch(function (error) {
+      console.error("Ocorreu um Erro.", error);
+    });
 }
 
-// Função para tirar foto 
-    cameraTrigger.onclick= function () {
-    cameraSensor.width = cameraView.videoWidth; 
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0); 
-    cameraOutput.src = cameraSensor, toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-    };
+// Função para tirar foto
+cameraTrigger.onclick = function () {
+  cameraSensor.width = cameraView.videoWidth;
+  cameraSensor.height = cameraView.videoHeight;
+  cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+  cameraOutput.src = cameraSensor.toDataURL("image/webp");
+  cameraOutput.classList.add("taken");
+};
 
-// carrege Imagem de camera quando a janela carregar 
+cameraSwitcher.onclick = function(){
+  stopMediaTracks(cameraView.srcObject);
+  camMode = camMode === "user" ? "enviroment" : "user";
+  constraints = { video: { facingMode: camMode }, audio: false};
+  console.log(constraints)
+  cameraStart();
+}
+
+function stopMediaTracks(stream) {
+  stream.getTracks().forEach(track => {
+    track.stop();
+  });
+}
+
+// carrega imagem de camera quando a janela carregar
 window.addEventListener("load", cameraStart, false);
